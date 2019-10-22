66961506d4eaeb15bb89ce9388d1481680aaa8e3
Baldvin B <baldvinbo@gmail.com>
2019-10-22 12:07:23 +0000

    Test fyrir Websocket real-time chat á nýju branchi chatversion

---
+++ src/main/java/is/hi/hbv501/bokamarkadur/bokamarkadur/WebsocketDemoApplication.java
@@ -0,0 +1,12 @@
+package is.hi.hbv501.bokamarkadur.bokamarkadur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketDemoApplication.class, args);
+    }
+}
