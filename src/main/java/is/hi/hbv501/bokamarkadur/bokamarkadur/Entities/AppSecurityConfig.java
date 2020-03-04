ab1ecd909a5f699d15137614dc14ff0ff2a675f7
sunnabj <sunnazool@gmail.com>
2020-03-04 23:03:33 +0000

    Set session timeout

--- src/main/java/is/hi/hbv501/bokamarkadur/bokamarkadur/Entities/AppSecurityConfig.java
+++ src/main/java/is/hi/hbv501/bokamarkadur/bokamarkadur/Entities/AppSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -31,6 +32,7 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter
         provider.setPasswordEncoder(new BCryptPasswordEncoder());
         return provider;
     }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
