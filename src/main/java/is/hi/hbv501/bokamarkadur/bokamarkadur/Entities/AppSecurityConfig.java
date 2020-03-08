4938602035ac80e3ff93dc54534a290e602999af
sunnabj <sunnazool@gmail.com>
2020-03-08 00:21:08 +0000

    Leyfa get síður án authenticate!

--- src/main/java/is/hi/hbv501/bokamarkadur/bokamarkadur/Entities/AppSecurityConfig.java
+++ src/main/java/is/hi/hbv501/bokamarkadur/bokamarkadur/Entities/AppSecurityConfig.java
@@ -80,7 +80,8 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter
                 // Allow POST request to to register and authenticate
                 .authorizeRequests().antMatchers(HttpMethod.POST,"/authenticate", "/register").permitAll().and()
                 // Allow GET requests to certain endpoints
-                .authorizeRequests().antMatchers(HttpMethod.GET, "/api/movies", "/api/rentals", "/api/users").permitAll()
+                .authorizeRequests().antMatchers(HttpMethod.GET, "/api/all-books", "/api/available-subjects",
+                "/api/viewbook/{id}", "/viewsubjectbooks/{subjects}", "/newest-books", "/available-subjects", "/users").permitAll()
                 // Require authentication for everything else
                 .anyRequest().authenticated().and()
                 // Extra stuff for error handling...
