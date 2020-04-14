ff14010cb973d6429e5bd9df50dcfd102bd4e9db
Hieu <hieutote@yahoo.com>
2020-04-14 22:49:00 +0000

    Hashing password for updatemyprofile

--- src/main/java/is/hi/hbv501/bokamarkadur/bokamarkadur/UserController.java
+++ src/main/java/is/hi/hbv501/bokamarkadur/bokamarkadur/UserController.java
@@ -10,10 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -125,6 +122,9 @@ public class UserController {
         current.setPhonenumber(user.phonenumber);
         
         if (user.password != null && user.password != "" && !user.password.isEmpty()) {
+            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+            String hashedPassword = passwordEncoder.encode(user.password);
+            user.password = hashedPassword;
             current.setPassword(user.password);
         }
         userService.save(current);
